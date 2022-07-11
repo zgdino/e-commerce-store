c58e9270ecc2969945b5e616495818c74a33b9c5
Dino Hadzic <zgdino@yahoo.com>
2022-07-11 11:26:52 +0100

    Setup HomePage from pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
+
+import { Home } from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>Online Store</h4>
+      <Home />
+    </div>
+  )
 }
 
 export default App
