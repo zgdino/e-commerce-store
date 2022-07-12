79349ad0cf049749aa10368c1cad1a750ca1fc71
Dino Hadzic <zgdino@yahoo.com>
2022-07-12 17:39:18 +0100

    Setup the navbar
    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,15 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return <NavContainer>
+    <div className="nav-center">
+      <div className="nav-header">
+        <Link to='/'>
+          <img src={logo} alt="store logo" />
+        </Link>
+      </div>
+    </div>
+  </NavContainer>
 }
 
 const NavContainer = styled.nav`
