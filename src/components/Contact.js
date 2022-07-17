2c3252c15577604aaa9710c80f56e39383aa387f
Dino Hadzic <zgdino@yahoo.com>
2022-07-17 12:35:22 +0100

    Build contact component on home page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,28 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+            repellat, ipsum recusandae similique ab quas ullam accusantium
+            aspernatur suscipit perferendis!
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type="submit" className='submit-btn'>subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
