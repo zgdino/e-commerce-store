208fa6550b2209537926d2aeb23b2c8f992a7218
Dino Hadzic <zgdino@yahoo.com>
2022-07-19 08:17:52 +0100

    Build Loading.js component spinner

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,6 +7,14 @@ import Loading from './Loading'
 import Product from './Product'
 
 const FeaturedProducts = () => {
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext()
+  if (loading) {
+    return <Loading />
+  }
   return <h4>featured products</h4>
 }
 
