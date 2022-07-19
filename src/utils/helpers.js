0aaa77ca500d913cfdb683cef81d8ad56b40cae4
Dino Hadzic <zgdino@yahoo.com>
2022-07-19 09:27:59 +0100

    Format the price of the products

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,10 @@
-export const formatPrice = () => {}
+// formating the price number to look up to standard since the original price is without any format
+export const formatPrice = (number) => {
+  const newNumber = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(number / 100)
+  return newNumber
+}
 
 export const getUniqueValues = () => {}
