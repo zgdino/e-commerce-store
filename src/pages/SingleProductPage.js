da38c53cfdca9ee36e9e8bd28f7af7acea5e78b9
Dino Hadzic <zgdino@yahoo.com>
2022-07-20 09:45:20 +0100

    Build Single Product Page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -59,6 +59,11 @@ const SingleProductPage = () => {
 
   return <Wrapper>
     <PageHero title={name} product/>
+    <div className="section section-center page">
+      <Link to='/products' className='btn'>
+        back to products
+      </Link>
+    </div>
   </Wrapper>
 }
 
