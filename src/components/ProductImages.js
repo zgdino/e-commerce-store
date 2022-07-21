0fb7ceb82040b8514404fb747cc639e9dba261b7
Dino Hadzic <zgdino@yahoo.com>
2022-07-21 14:40:50 +0100

    Build ProductImages.js component
    Gallery functionality onClick

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -6,10 +6,10 @@ const ProductImages = ({ images = [{url: ''}] }) => {
   const [main, setMain] = useState(images[0])
 
   return <Wrapper>
-    <img src={main.url}  alt="main image" className='main' />
+    <img src={main.url}  alt="main image" className='' />
     <div className="gallery">
       {images.map((image, index) => {
-        return <img src={image.url} alt="" />
+        return <img src={image.url} alt={image.filename} key={index} onClick={() => setMain(images[index])} />
       })}
     </div>
   </Wrapper>
