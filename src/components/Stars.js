02aee234e8efed69fcae2a9c3e36e28e90959c66
Dino Hadzic <zgdino@yahoo.com>
2022-07-22 11:31:07 +0100

    Refactor star rating functionality

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,66 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
 const Stars = ({ stars, reviews }) => {
-  console.log(stars, reviews)
+  // {
+  //   /* star setup */
+  // }
+  // <span>
+  //   {stars >= 1 ? <BsStarFill /> : stars >= 0.5 ? <BsStarHalf /> : <BsStar />}
+  // </span>
+  // {
+  //   /* end star setup */
+  // }
+
+  // creating an array with the length of 5
+  const tempStars = Array.from({ length: 5 }, (_, index) => {
+    const number = index + 0.5
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    )
+  })
+
   return (
     <Wrapper>
-      <div className='stars'>
-        {/* star setup */}
-        <span>
-          {stars >= 1 ? (
-            <BsStarFill />
-          ) : stars >= 0.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end star setup */}
-        {/* star setup */}
-        <span>
-          {stars >= 2 ? (
-            <BsStarFill />
-          ) : stars >= 1.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end star setup */}
-        {/* star setup */}
-        <span>
-          {stars >= 3 ? (
-            <BsStarFill />
-          ) : stars >= 2.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end star setup */}
-        {/* star setup */}
-        <span>
-          {stars >= 4 ? (
-            <BsStarFill />
-          ) : stars >= 3.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end star setup */}
-        {/* star setup */}
-        <span>
-          {stars >= 5 ? (
-            <BsStarFill />
-          ) : stars >= 4.5 ? (
-            <BsStarHalf />
-          ) : (
-            <BsStar />
-          )}
-        </span>
-        {/* end star setup */}
-      </div>
+      <div className='stars'>{tempStars}</div>
       <p className='reviews'>({reviews} customer reviews)</p>
     </Wrapper>
   )
