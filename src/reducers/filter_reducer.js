c3714d1d2ebe5031651b2e721aae2ad1761eaf79
Dino Hadzic <zgdino@yahoo.com>
2022-07-23 12:45:48 +0100

    Setup the filter_reducer.js

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,7 +11,13 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
-    return { ...state, all_products: [...action.payload], filter_products:[...action.payload]}
+    return {
+      ...state,
+      // copying all the values from the payload, not referencing
+      all_products: [...action.payload],
+      // setting it to be equal to all_products in the beginning because at this point program is loading the products OR user has just entered the page so we want to show all of the products without any filters on it. Later the filter will be applied on filter_products
+      filtered_products: [...action.payload],
+    }
   }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
