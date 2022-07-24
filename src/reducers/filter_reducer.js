c56368ff1e76b25467f5dc47751f527e8a6dda8e
Dino Hadzic <zgdino@yahoo.com>
2022-07-24 07:27:07 +0100

    Build ProductsPage.js - initial setup

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,7 +13,7 @@ const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
     return {
       ...state,
-      // copying all the values from the payload, not referencing
+      // copying all the values from the payload, not referencing → this will be the deafult value
       all_products: [...action.payload],
       // setting it to be equal to all_products in the beginning because at this point program is loading the products OR user has just entered the page so we want to show all of the products without any filters on it. Later the filter will be applied on filter_products
       filtered_products: [...action.payload],
