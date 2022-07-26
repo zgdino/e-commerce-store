b29919702cf73777fa64f6210125f5eedd0d6587
Dino Hadzic <zgdino@yahoo.com>
2022-07-26 09:39:23 +0100

    Update LOAD_PRODUCTS action in filter_reducer.js
    Asign maxPrice to be max_price and default price

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -10,16 +10,19 @@ import {
 } from '../actions'
 
 const filter_reducer = (state, action) => {
+  // CONDITIONALS
   if (action.type === LOAD_PRODUCTS) {
     let maxPrice = action.payload.map((p) => p.price)
     maxPrice = Math.max(...maxPrice)
-    console.log(maxPrice)
+
     return {
       ...state,
       // copying all the values from the payload, not referencing → this will be the deafult value
       all_products: [...action.payload],
       // setting it to be equal to all_products in the beginning because at this point program is loading the products OR user has just entered the page so we want to show all of the products without any filters on it. Later the filter will be applied on filter_products
       filtered_products: [...action.payload],
+      // after defining what maxPrice is, asign its value to the state → every time website loads, max_price and initial price will be eqaul to maxPrice
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     }
   }
 
@@ -60,6 +63,8 @@ const filter_reducer = (state, action) => {
         return b.name.localeCompare(a.name)
       })
     }
+
+    // END CONDITIONALS
 
     return { ...state, filtered_products: tempProducts }
   }
