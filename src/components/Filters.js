19e9a0c64c3be7b98402299c7adedb3b38a2e4f6
Dino Hadzic <zgdino@yahoo.com>
2022-07-29 10:51:10 +0100

    Build price filter in Filter.js
    Build the basic setup

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -128,6 +128,13 @@ const Filters = () => {
             </div>
           </div>
           {/* end of colors */}
+          {/* price */}
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input type="radio" name="price" onChange={updateFilters} min={min_price} max={max_price} value={price} />
+          </div>
+          {/* end of price */}
         </form>
       </div>
     </Wrapper>
