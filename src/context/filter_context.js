09ed9433eeed3790ceab9b792bf2f61c27d5af5f
Dino Hadzic <zgdino@yahoo.com>
2022-07-29 11:41:54 +0100

    Update updateFilters function in filter_context
    Add conditional if dealing with shipping

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -75,6 +75,9 @@ export const FilterProvider = ({ children }) => {
     if (name === 'price') {
       value = Number(value)
     }
+    if (name === 'shipping') {
+      value = e.target.checked
+    }
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
   }
 
