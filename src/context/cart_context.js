35f81f845c7009a71d51f23ecc351f8203359f21
Dino Hadzic <zgdino@yahoo.com>
2022-08-01 11:30:44 +0100

    Build addToCart function in cart_context.js

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -20,8 +20,17 @@ const CartContext = React.createContext()
 export const CartProvider = ({ children }) => {
   // reducer coming from cart_reducer
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  // add to cart
+  const addToCart = (id, color, amount, product) => {
+    // remember id:id === id - in payload ES6
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
+  }
+
   return (
-    <CartContext.Provider value={{...state, }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 // make sure use
