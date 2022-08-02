2d7da0309e03a672a4710904e9b63a255ae7f851
Dino Hadzic <zgdino@yahoo.com>
2022-08-02 17:35:13 +0100

    Build ADD_TO_CART action in cart_reducer.js

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,7 +7,27 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
-  return state
+  if (action.type === ADD_TO_CART) {
+    const {id, color, amount, product} = action.payload
+    // return only those items whose id matches with the ones that have the same id + color - if it matches, it means that item is already in the cart, if it does not, we are adding a brand new item
+    const tempItem = state.cart.find((i) => i.id === id + color)
+    if (tempItem) {
+
+    }
+    else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+      return {...state, cart:[...state.cart, newItem]}
+    }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
