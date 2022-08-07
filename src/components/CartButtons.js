8c00cf51268005b1ad0729037cf9c983effb3c1a
Dino Hadzic <zgdino@yahoo.com>
2022-08-07 09:53:49 +0100

    Update login/logout button in CartButtons.js

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -9,7 +9,7 @@ import { useUserContext } from '../context/user_context'
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
   const { total_items } = useCartContext()
-  const { loginWithRedirect, muUser, logout } = useUserContext()
+  const { loginWithRedirect, myUser, logout } = useUserContext()
   // adding class to be screen responsive(hide/display)
   return (
     <Wrapper className='cart-btn-wrapper'>
@@ -20,17 +20,21 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type='button' className='auth-btn' onClick={loginWithRedirect}>
-        Login <FaUserPlus />
-      </button>
-      <button
-        type='button'
-        className='auth-btn'
-        // from Auth0 documentation
-        onClick={() => logout({ returnTo: window.location.origin })}
-      >
-        Logout <FaUserMinus />
-      </button>
+      {/* if user logged in, show logout button only and vice versa */}
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          // from Auth0 documentation
+          onClick={() => logout({ returnTo: window.location.origin })}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
     </Wrapper>
   )
 }
