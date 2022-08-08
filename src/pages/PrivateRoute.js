0a7863c6d75e7bcc5bb8ef3f5526b07a15797fd8
Dino Hadzic <zgdino@yahoo.com>
2022-08-08 10:47:46 +0100

    Update PrivateRoute with useAuth0

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,6 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { useUserContext } from '../context/user_context'
 
 const PrivateRoute = ({ children, ...rest }) => {
+  // getting the user from useAuth0
   const {user} = useAuth0()
   return <Route {...rest} render={() => {
     return user ? children : <Redirect to='/'></Redirect>
