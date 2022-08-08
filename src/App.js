cc367164d5970622454b7d2a41db38cd38a9b525
Dino Hadzic <zgdino@yahoo.com>
2022-08-08 11:01:03 +0100

    Update App.js with AuthWrapper
    Wrapping the whole application with AuthWrapper

--- src/App.js
+++ src/App.js
@@ -12,39 +12,42 @@ import {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper,
 } from './pages'
 
 function App() {
   // Navbar, Sidebar, (something inbetween) and Footer are allways displayed no matter what → that is why they are out of the router
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
-        <Route exact path='/products'>
-          <Products />
-        </Route>
-        {/* route for SingleProducts is different - single-ing out specific product out of Products ... determined by :id; SingleProduct will have functionality to determine which product to be shown*/}
-        <Route exact path='/products/:id' children={<SingleProduct />} />
-        {/* Checkout component will eventually be wrapped in a PrivateRoute making sure that only logged in visitors have access */}
-        <PrivateRoute exact path='/checkout'>
-          <Checkout />
-        </PrivateRoute>
-        <Route path='*'>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <Route exact path='/products'>
+            <Products />
+          </Route>
+          {/* route for SingleProducts is different - single-ing out specific product out of Products ... determined by :id; SingleProduct will have functionality to determine which product to be shown*/}
+          <Route exact path='/products/:id' children={<SingleProduct />} />
+          {/* Checkout component will eventually be wrapped in a PrivateRoute making sure that only logged in visitors have access */}
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   )
 }
 
