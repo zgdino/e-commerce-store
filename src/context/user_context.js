900a1311030ddb232a6a516c61e45a4d30af9808
Dino Hadzic <zgdino@yahoo.com>
2022-08-08 10:46:33 +0100

    Update PrivateRoute with useAuth0

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,19 +3,14 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+  const { loginWithRedirect, logout, user } =
     useAuth0()
 
   const [myUser, setMyUser] = useState(null)
 
   useEffect(() => {
-    if (isAuthenticated) {
-      // user is coming from useAuth0 hook
-      setMyUser(user)
-    } else {
-      setMyUser(false)
-    }
-  }, [isAuthenticated])
+   setMyUser(user)
+  }, [user])
 
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
