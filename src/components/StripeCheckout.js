d2f276ac97640f11759c7b8fb8f6f4ba2ba6f628
Dino Hadzic <zgdino@yahoo.com>
2022-08-28 12:33:20 +0100

    Build StripeCheckout messages

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -67,8 +67,32 @@ const CheckoutForm = () => {
     // eslint-disable-next-line
   }, [])
 
-  const handleChange = async (event) => {}
-  const handleSubmit = async (ev) => {}
+  const handleChange = async (event) => {
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
+  }
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      // after successful payment it will clear the cart and redirect to the home page after 10 seconds
+      setTimeout(() => {
+        clearCart()
+        history.push('/')
+      }, 10000)
+    }
+  }
 
   return (
     <div>
